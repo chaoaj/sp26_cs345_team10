20ad77ee392363cf56dd22d20d616b069baa6ebb
Tony <aatony018@proton.me>
2026-04-01 10:52:57 -0400

    Fixed rockLevel.js to work with updated level switching

--- dev/rockLevel.js
+++ dev/rockLevel.js
@@ -1,21 +1,11 @@
-let levelMusic;
 let projectiles = []; // holds the projectiles being shot
 let player_x = 300;
 let player_y = 300; // this can be changed when the player class is ready
 
-function setup() {
-  createCanvas(960, 540);
-  x = 0;
-  playLevelMusic();
-}
-
-function preload() {
-  back = loadImage('Assets/Test_Level_Lava.png');
-  levelMusic = loadSound('Assets/Music/Terrible_Placeholder_Music.mp3');
-}
-
-function draw() {
-  image(back, 0, 0, 960, 540);
+  
+function rockDraw() {
+  image(metal_back, 0, 0, 960, 540);
+  // debug stuff
   circle(player_x, player_y, 50); // temporary character 
   for (let i = 0; i < projectiles.length; i++) {
     projectiles[i].update();
@@ -25,12 +15,6 @@ function draw() {
       projectiles.splice(i, 0)
     }
   }
-}
-
-function playLevelMusic() {
-  levelMusic.play();
-  levelMusic.setVolume(0.3); // change the volume between 0.0 and 1.0 if needed
-  userStartAudio();
 }
 
 function mousePressed() {
